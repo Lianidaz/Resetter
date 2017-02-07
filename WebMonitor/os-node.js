d5266d18e6d94a14264dc8c7dd9280049c4cf512
Lianidaz <lianidaz@gmail.com>
2017-02-07 18:43:53 +0300

    node

--- WebMonitor/os-node.js
+++ WebMonitor/os-node.js
@@ -3,9 +3,20 @@ var mqtt = require('mqtt');
 
 var client = mqtt.connect("mqtt://admin:Q!w2e3r4@10.0.1.5","os-mon");
 
-var post = os.userInfo().username;
+var topic = os.hostname();
+var post = os.userInfo().username + "|" + os.platform();
 
-client.on('connect', function() {
-  client.publish(os.hostname(), post);
+var d = Date.now();
+
+client.on('connect', function(){
   client.subscribe('#');
-})
+  client.publish(topic, post);
+  // console.log("connected! " + post);
+  // while (true) {
+    d = Date.now();
+    if ((d % 60000) == 0) {
+      client.publish(os.hostname(), post);
+      // console.log(post);
+    }
+  // }
+});
